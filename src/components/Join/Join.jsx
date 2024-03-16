2cf4c4c57ad20087775eef15d1c585df81d66975
Shwet Rana <shwetrana980@gmail.com>
2024-03-16 21:22:00 +0530

    first commit

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,53 @@
+import React, { useRef } from "react";
+import "./Join.css";
+import emailjs from "@emailjs/browser";
+const Join = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_gw3fs58",
+        "template_pi1xk1w",
+        form.current,
+        "YC1fNjdQ6hiLfAq5W"
+      )
+      .then(
+        (result) => {
+          console.log("SUCCESS!", result.text);
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+  return (
+    <div className="Join" id="join-us">
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className="stroke-text">READY TO</span>
+          <span>LEVEL UP</span>
+        </div>
+        <div>
+          <span>YOUR BODY</span>
+          <span className="stroke-text">WITH US?</span>
+        </div>
+      </div>
+      <div className="right-j">
+        <form ref={form} className="email-container" onSubmit={sendEmail}>
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Enter your email address"
+          />
+          <button className="btn btn-j">Join Now</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
